a5a6aeb9f985b1526f3bbbb8d287d5431112f475
Clayton Ray <iamclaytonray@gmail.com>
2020-07-26 17:15:49 -0400

    fix: more stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,6 @@ exports.createPages = ({ actions, graphql }) => {
               slug
             }
             frontmatter {
-              tags
               templateKey
             }
           }
@@ -36,7 +35,9 @@ exports.createPages = ({ actions, graphql }) => {
       createPage({
         path: edge.node.fields.slug,
         tags: edge.node.frontmatter.tags,
-
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`,
+        ),
         // additional data can be passed via context
         context: {
           id,
